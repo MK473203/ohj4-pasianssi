97a47e2252767cf77ee373c0e18ba790e309eb97
Markus <mk473203@outlook.com>
2023-04-11 16:58:59 +0300

    Added basic files

---
+++ src/main/java/com/ohjelmointi4/App.java
@@ -0,0 +1,13 @@
+package com.ohjelmointi4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
