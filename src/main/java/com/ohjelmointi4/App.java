649a4bc11112ec12da55661e622cc301cd7b915f
Markus <mk473203@outlook.com>
2023-04-11 18:34:04 +0300

    Näkymävaihdoksien aloittelua

--- src/main/java/com/ohjelmointi4/App.java
+++ src/main/java/com/ohjelmointi4/App.java
@@ -1,71 +1,93 @@
 package com.ohjelmointi4;
 
 import javax.swing.*;
-
+import java.awt.Canvas;
 import java.awt.CardLayout;
 import java.awt.Container;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App extends JFrame implements ActionListener {
 
-         JFrame f=new JFrame("pasianssipeli :)");//creating instance of JFrame  
+    CardLayout crd;
 
-        CardLayout crd = new CardLayout();
+    Container cPane;
 
-        Container cPane = f.getContentPane();    
-        cPane.setLayout(crd);    
+    // constructor of the class
+    App() {
 
-        
-       
+        setSize(800, 600);
 
+        cPane = getContentPane();
+
+        // default constructor used
+        // therefore, components will
+        // cover the whole area
+        crd = new CardLayout();
+
+        cPane.setLayout(crd);
+
+        Container mainMenuContainer = new Container();
+        Container leaderboardContainer = new Container();
+        Container settingsContainer = new Container();
 
         ImageIcon icon = new ImageIcon("/nimetön.png");
 
-        f.setIconImage(icon.getImage());
-        f.setResizable(false);
-                
-        JButton a=new JButton("uusi peli");//creating instance of JButton  
-        a.setBounds(100,100,100, 40);//x axis, y axis, width, height  
-                
-        JButton b=new JButton("pisteet");
-        b.setBounds(100,200,100, 40);
+        setIconImage(icon.getImage());
+        setResizable(false);
 
-        JButton c=new JButton("asetukset");
-        c.setBounds(100,300,100, 40);
+        JButton a = new JButton("Uusi peli");
+        a.setBounds((getWidth() / 2) - (getWidth() / 4), 100, (getWidth() / 2), 40);
 
-        JButton d=new JButton("ohjeet");
-        d.setBounds(100,400,100, 40);
+        JButton b = new JButton("Pisteet");
+        b.setBounds(350, 200, 100, 40);
 
-        Rectangle r = new Rectangle(600, 100, 100, 100);
+        JButton c = new JButton("Asetukset");
+        c.setBounds(350, 300, 100, 40);
 
-        JSlider e = new JSlider(JSlider.HORIZONTAL,0, 100, 10);
-        e.setBounds(600, 100, 100, 100);
+        JButton d = new JButton("Poistu");
+        d.setBounds(350, 400, 100, 40);
 
-        f.add(a);
-        f.add(b);//adding button in JFrame  
-        f.add(c);
-        f.add(d);
-        f.add(e);
-                
+        // adding listeners to it
+        a.addActionListener(this);
+        b.addActionListener(this);
+        c.addActionListener(this);
 
-        f.setSize(800,600);//400 width and 500 height  
-        f.setLayout(null);//using no layout managers  
-        f.setVisible(true);//making the frame visible  
+        mainMenuContainer.add(a);
+        mainMenuContainer.add(b);
+        mainMenuContainer.add(c);
+        mainMenuContainer.add(d);
 
-        cPane.add("a", a); // first card is the button btn1  
-        cPane.add("b", b); // first card is the button btn2  
-        cPane.add("c", c);  // first card is the button btn3  
+        cPane.add("main menu", mainMenuContainer);
+        cPane.add("leaderboards", leaderboardContainer);
+        cPane.add("settings", settingsContainer);
 
 
+    }
 
-        System.out.println( "Hello World!" );
+    public void actionPerformed(ActionEvent e) {
+        // Upon clicking the button, the next card of the container is shown
+        // after the last card, again, the first card of the container is shown upon clicking
+
+        if (e.getActionCommand() == "Uusi peli") {
+            crd.show(cPane, "leaderboards");
+        }
+
+    }
+
+    // main method
+    public static void main(String argvs[]) {
+        // creating an object of the class CardLayoutExample1
+        App crdl = new App();
+
+        // size is 300 * 300
+        crdl.setVisible(true);
+        crdl.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
